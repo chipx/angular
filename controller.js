1be2abd3816cd01cb43133fc479de677834bb723
chipx <melnikov.vikt@gmail.com>
2014-09-22 23:14:39 +0400

    updatte

--- controller.js
+++ controller.js
@@ -1,48 +1,17 @@
-function OrderFormController($scope){
+function MainController($scope){
+    $scope.contact = {
+        'firstName': '',
+        'lastName': ''
+    };
 
-    // Определение параметров модели. Вид отображения будет зависеть от
-    // обработки циклом массива перечня услуг, для которых автоматически будет	// сгенерирован формат вывода списком через тэг li.
-
-    $scope.services = [
+    $scope.contacts = [
         {
-            name: 'Web Development',
-            price: 300,
-            active:true
-        },{
-            name: 'Design',
-            price: 400,
-            active:false
-        },{
-            name: 'Integration',
-            price: 250,
-            active:false
-        },{
-            name: 'Training',
-            price: 220,
-            active:false
+            'firstName': 'Ivanov',
+            'lastName': 'Ivan'
+        },
+        {
+            'firstName': 'Petrov',
+            'lastName': 'Petr'
         }
-    ];
-
-    $scope.toggleActive = function(s){
-        s.active = !s.active;
-    };
-
-    // Вспомогательный метод подсчета общей суммы по всем выбранным
-    // позициям.
-
-    $scope.total = function(){
-
-        var total = 0;
-
-        // Use the angular forEach helper method to
-        // loop through the services array:
-
-        angular.forEach($scope.services, function(s){
-            if (s.active){
-                total+= s.price;
-            }
-        });
-
-        return total;
-    };
+    ]
 }
